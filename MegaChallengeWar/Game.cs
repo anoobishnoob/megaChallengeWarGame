333ec49bbc54dcc71747d79b0ea849a7aa0273b3
Samme Qandil <Saqandil@gmail.com>
2017-12-08 15:29:04 -0800

    Add files via upload

---
+++ MegaChallengeWar/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar
+{
+    public class Game
+    {
+        private Player _player1;
+        private Player _player2;
+       
+
+        public Game(string player1Name, string player2Name)
+        {
+            _player1 = new Player() { Name = player1Name };
+            _player2 = new Player() { Name = player2Name };
+            
+            //Deck deck = new Deck();
+
+        
+        }
+        public string Play()
+        {
+            Deck deck = new Deck();
+
+            string result = "<h3>Dealing cards... </h3>";
+            result += deck.Deal(_player1, _player2);
+
+            result += "<h3>Begin Battle... </h3>";
+            int round = 0;
+            while (_player1.Cards.Count != 0 && _player2.Cards.Count != 0)
+            {
+
+                Battle battle = new Battle();
+                result += battle.performBattle(_player1, _player2);
+
+                round++;
+                if (round > 20)
+                    break;
+
+            }
+            // determine the winner
+            result += determineWinner();
+            return result;
+        }
+        
+       // private List<Card> _bounty;
+        private string determineWinner()
+        {
+            string result = "";
+            if (_player1.Cards.Count > _player2.Cards.Count)
+                result += "<br/><span style='color:red;font-weight:bolder;'>Player 1 wins!</span>";
+            else if (_player2.Cards.Count > _player1.Cards.Count)
+                result += "<br/><span style='color:blue;font-weight:bolder;'> Player 2 wins!</span>";
+            else
+                result += "<br/><span style='font-weight:bolder;'> Game is a draw!</span>";
+                // noticed that I had a draw, and player 2 won, so I expanded the logic
+
+            result += "<br/><span style='color:red;font-weight:bolder;'> Player1:" + _player1.Cards.Count + "</span> <br/><span style='color:blue;font-weight:bolder;'>Player2: "+ _player2.Cards.Count +"</span>";
+            return result;
+        }
+
+    }
+}
---
+++ MegaChallengeWar/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaChallengeWar
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void playButton_Click(object sender, EventArgs e)
+        {
+            /* Deck deck = new Deck(); for testing my deck list
+             foreach (var card in deck._deck)
+             {
+                 resultLabel.Text += "<br/>" + card.Suit + " " + card.Kind;
+             } */
+            Game game = new Game("Player1", "Player2");
+            resultLabel.Text = game.Play();
+
+        }
+    }
+}
